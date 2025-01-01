dd7ed0b47c87150ff51e65c4eb5f38d9aef82a27
Piyush <rana.workslance@gmail.com>
2025-01-01 18:36:35 +0530

    Addons

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import './globals.css';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
 
 const App = () => {
-  return (
-    <main className="flex h-screen">
-        <Routes>
-            {/* Public Routes */}
-            <Route path='/sign-in' element={<SignInForm />} />
+    return (
+        <main className="flex h-screen">
+            <Routes>
+                {/* Public Routes */}
+
+                <Route element={<AuthLayout />}>
+
+                </Route>
+                <Route path='/sign-in' element={<SignInForm />} />
+                <Route path='/sign-up' element={<SignUpForm />} />
 
 
-            {/* Private Routes */}
-            <Route index element={<Home />} />
+                {/* Private Routes */}
+                <Route element={<RootLayout />}>
 
-        </Routes>
-    </main>
-  )
+                    <Route index element={<Home />} />
+                </Route>
+
+            </Routes>
+        </main>
+    )
 }
 
 export default App
